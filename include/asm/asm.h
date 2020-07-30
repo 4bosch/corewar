ce19b9157720cfb9961a1186ee7c8dab0689ba65
aboscho <argan.bosch@protonmail.com>
2020-07-30 11:30:23 +0200

    chore: add .h into asm.h

--- include/asm/asm.h
+++ include/asm/asm.h
@@ -6,7 +6,7 @@
 /*   By: abaisago <adam_bai@adam@tuta.io>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 16:31:58 by abaisago          #+#    #+#             */
-/*   Updated: 2020/06/29 20:07:04 by abosch           ###   ########.fr       */
+/*   Updated: 2020/07/30 11:26:33 by abosch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@
 
 # include "util.h"
 # include "lexer.h"
-# include "parser.h"
 # include "error.h"
+# include "parser.h"
+# include "translator.h"
 
 int		asmcore(int ac, char **av);
 
